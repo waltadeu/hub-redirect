4b8702bc850591a0a741698368bdca45c5faaf10
Walter Tadeu <waltadeu@gmail.com>
2026-08-18 09:40:49 -0300

    Update i18n.js

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -121,6 +121,10 @@ window.HR_I18N = {
     'pf.new.pass': 'Nova senha',
     'pf.change.pass': 'Alterar senha',
     'pf.saved': 'Alterado com sucesso!',
+    //FAQ
+    'faq.subtitle': 'FAQ',
+    'faq.title': 'Perguntas Frequentes',
+    'faq.sub': 'Tire todas as suas dúvidas, e compre com segurança.'
   },
   'en': {
     'nav.home': 'Home',
@@ -239,6 +243,10 @@ window.HR_I18N = {
     'pf.new.pass': 'New password',
     'pf.change.pass': 'Change password',
     'pf.saved': 'Saved successfully!',
+    //FAQ
+    'faq.subtitle': 'FAQ',
+    'faq.title': 'Frequently Asked Questions'
+    'faq.sub': 'Get all your questions answered, and buy with confidence.'
   }
 };
 
